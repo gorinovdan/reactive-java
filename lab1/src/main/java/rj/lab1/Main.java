602fbcb74c1d8d827d2e40067275295824067c29
Alexander Shcherbakov <Neegroom@yandex.ru>
2025-09-22 00:01:31 +0300

    add first aggregator

--- lab1/src/main/java/rj/lab1/Main.java
+++ lab1/src/main/java/rj/lab1/Main.java
@@ -2,7 +2,10 @@ package rj.lab1;
 
 import rj.lab1.generators.SimpleReceiptGenerator;
 import rj.lab1.model.Receipt;
+import rj.lab1.statistics.ReceiptStatisticsIterateCircleAggregator;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 public class Main {
@@ -11,8 +14,15 @@ public class Main {
                 .withItemRange(2, 7)
                 .withPriceRange(5.0, 200.0);
 
-        List<Receipt> receipts = generator.generateMany(2000);
+        List<Receipt> receipts = generator.generateMany(250000);
 
-        System.out.println(receipts.size());
+        Instant start = Instant.now();
+        var stat = ReceiptStatisticsIterateCircleAggregator.aggregate(receipts);
+        Instant end = Instant.now();
+
+        Duration duration = Duration.between(start, end);
+
+        System.out.println(stat);
+        System.out.println("Duration: " + duration.toMillis() + " ms");
     }
 }
--- lab1/src/main/java/rj/lab1/statistics/ReceiptStatistics.java
+++ lab1/src/main/java/rj/lab1/statistics/ReceiptStatistics.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import rj.lab1.model.ReceiptStatus;
 
 import java.util.Map;
+import java.util.StringJoiner;
 
 @Setter
 @Getter
@@ -18,13 +19,24 @@ public class ReceiptStatistics {
     private Map<ReceiptStatus, Long> ordersByStatus;
 
     private long totalItemsSold;
-    private Map<String, Long> itemsByQuantity;
-    private Map<String, Double> itemsByRevenue;
-
     private long uniqueCustomers;
-    private Map<String, Double> revenueByCustomer;
-
     private long totalLoyaltyPoints;
     private Map<Integer, Double> revenueByMonth;
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner("\n  ", "ReceiptStatistics{\n  ", "\n}");
+        joiner.add("totalRevenue=" + totalRevenue);
+        joiner.add("averageReceiptAmount=" + averageReceiptAmount);
+        joiner.add("minReceiptAmount=" + minReceiptAmount);
+        joiner.add("maxReceiptAmount=" + maxReceiptAmount);
+        joiner.add("totalOrders=" + totalOrders);
+        joiner.add("ordersByStatus=" + ordersByStatus);
+        joiner.add("totalItemsSold=" + totalItemsSold);
+        joiner.add("uniqueCustomers=" + uniqueCustomers);
+        joiner.add("totalLoyaltyPoints=" + totalLoyaltyPoints);
+        joiner.add("revenueByMonth=" + revenueByMonth);
+        return joiner.toString();
+    }
 }
 
